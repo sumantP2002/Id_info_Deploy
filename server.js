f066d2d99e9638e178e625e647eef567a725c00a
Sumant Kumar Pandey <96868383+sumantP2002@users.noreply.github.com>
2023-12-25 22:34:44 +0530

    update2

--- server.js
+++ server.js
@@ -4,45 +4,49 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
 import apiRoutes from "./Routes/apiRoutes.js";
-const path = require('path');
-
+import { fileURLToPath } from "url";
+import path from "path";
 
 dotenv.config();
-const DB=process.env.MONGO_URL;
+const DB = process.env.MONGO_URL;
+
+// Get the directory name using the import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 try {
-    console.log('hello');
-    await mongoose.connect(DB, {});
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
-  }
+  console.log('hello');
+  await mongoose.connect(DB, {});
+  console.log('Connected to MongoDB');
+} catch (error) {
+  console.error('Error connecting to MongoDB:', error.message);
+}
 
-const app=express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 console.log("inside server");
-app.use('/api',apiRoutes);
+app.use('/api', apiRoutes);
 
-//static files
-app.use(express.static(path.join(__dirname, '../client/build')))
-app.get('*' , function(req , res){
-  res.sendFile(path.join(__dirname, "../client/build/index.html"))
-})
-
-app.get("/",(req,res)=>{
-    res.send("<h1>Welcome To DreamWed- Connect</h1>")
+// Static files
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-const PORT= process.env.PORT || 8000;
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome To DreamWed- Connect</h1>");
+});
 
-//3 step heroku
-if(process.env.NODE_ENV == "production"){
+const PORT = process.env.PORT || 8000;
+
+// Heroku deployment step
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.listen(PORT,()=>{
-    console.log(`Server running at ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
+});
